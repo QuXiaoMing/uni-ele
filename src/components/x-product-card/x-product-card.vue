9ca32eab0f77cb200ca92532f594b5c5953f0c41
QuXiaoMing <xm726@qq.com>
2019-07-16 11:31:36 +0800

    【增加】折叠

--- src/components/x-product-card/x-product-card.vue
+++ src/components/x-product-card/x-product-card.vue
@@ -15,20 +15,50 @@
                 </view>
             </view>
         </view>
+        <view class="x-product-card__activities">
+            <view class="x-product-card__activities_list">
+                <view class="x-product-card__activities_items" v-for="(item, index) in activities" :key="index">
+                    <image :src="item.logo"></image> {{item.name}}
+                </view>
+            </view>
+            <view class="x-product-card__activities_trigger" @click="isFold = !isFold">
+                {{data.activities.length}} 个活动
+                <uni-icon v-if="!isFold" type="arrowup" color="#333" size="12"></uni-icon>
+                <uni-icon v-if="isFold" type="arrowdown" color="#333" size="12"></uni-icon>
+            </view>
+        </view>
     </view>
 </template>
 
 <script>
 export default {
+    data() {
+        return {
+            isFold: true
+        };
+    },
     props: {
         data: {
             type: Object,
             default() {
                 return {
                     logo: '/static/ele/icons/crown.png',
-                    name: '名字'
+                    name: '名字',
+                    activities: [
+                        { name: 'asdasd', logo: '/static/ele/icons/crown.png' },
+                        { name: 'asdasd', logo: '/static/ele/icons/crown.png' },
+                        { name: 'asdasd', logo: '/static/ele/icons/crown.png' }
+                    ]
                 };
             }
+        }
+    },
+    computed: {
+        activities() {
+            if (Array.isArray(this.data.activities)) {
+                return this.data.activities.filter((e, index) => (this.isFold ? index < 2 : true));
+            }
+            return [];
         }
     }
 };
@@ -62,6 +92,23 @@ export default {
         line-height: 16px;
         margin-top: 1px;
         margin-bottom: 4px;
+    }
+    &__activities {
+        display: flex;
+    }
+    &__activities_list {
+        flex: 1;
+    }
+    &__activities_items {
+        font-size: 12px;
+        image {
+            width: 12px;
+            height: 12px;
+            margin-right: 5px;
+        }
+    }
+    &__activities_trigger {
+        font-size: 12px;
     }
 }
 </style>
